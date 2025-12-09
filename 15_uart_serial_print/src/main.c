02822f107ad027ebecb03b03b06aace7a119e088
Mark Kamal <mark.kamal93@gmail.com>
2025-12-09 18:44:43 +0200

    dht22 end

--- 15_uart_serial_print/src/main.c
+++ 15_uart_serial_print/src/main.c
@@ -7,8 +7,6 @@
 
 void app_main() {
     const char *msg = "hello world from uart\n";
-
-
     uart_write_bytes(UART_NUM_0 , msg , strlen(msg));
     vTaskDelay(pdMS_TO_TICKS(1000));
 
---
+++ 46_DHT22_interfacing/components/dht22/DHT22.h
@@ -0,0 +1,29 @@
+/* 
+	DHT22 temperature sensor driver
+*/
+
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+
+#ifndef DHT22_H_  
+#define DHT22_H_
+
+#define DHT_OK 0
+#define DHT_CHECKSUM_ERROR -1
+#define DHT_TIMEOUT_ERROR -2
+
+// == function prototypes =======================================
+
+void 	setDHTgpio(int gpio);
+void 	errorHandler(int response);
+int 	readDHT();
+float 	getHumidity();
+float 	getTemperature();
+int 	getSignalLevel( int usTimeOut, bool state );
+
+#endif
